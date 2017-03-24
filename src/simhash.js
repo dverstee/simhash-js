f747ddec3aa5d0f825e7dfee0d97846264f8bbc2
Dimitry Versteele <dimitry.versteele@vasco.com>
2017-03-24 11:00:23 +0100

    I have put the similarity and hamming distance in seperate functions since they are independent from the Simhash.
    This also fixes the problem at issue #1. I think this can be closed now.

--- src/simhash.js
+++ src/simhash.js
@@ -103,45 +103,6 @@ var Simhash = function(options) {
     };
 
     /**
-     * Calculates binary hamming distance of two base 16 integers.
-     */
-    var hammingDistance = function(x, y) {
-        var a1 = parseInt(x, 16);
-        var a2 = parseInt(y, 16);
-	    var v1 = a1^a2;
-	    var v2 = (a1^a2)>>32;
-
-	    v1 = v1 - ((v1>>1) & 0x55555555);
-	    v2 = v2 - ((v2>>1) & 0x55555555);
-	    v1 = (v1 & 0x33333333) + ((v1>>2) & 0x33333333);
-	    v2 = (v2 & 0x33333333) + ((v2>>2) & 0x33333333);
-	    var c1 = ((v1 + (v1>>4) & 0xF0F0F0F) * 0x1010101) >> 24;
-	    var c2 = ((v2 + (v2>>4) & 0xF0F0F0F) * 0x1010101) >> 24;
-
-	    return c1 + c2;
-    };
-
-    /**
-     * Calculates bit-wise similarity - Jaccard index.
-     */
-    var similarity = function(x, y) {
-        var x16 = parseInt(x, 16);
-        var y16 = parseInt(y, 16);
-        var i = (x16 & y16);
-        var u = (x16 | y16);
-        return hammingWeight(i) / hammingWeight(u);
-    };
-
-    /**
-     * Calculates Hamming weight (population count).
-     */
-    var hammingWeight = function(l) {
-        var c;
-        for(c = 0; l; c++) l &= l-1;
-        return c;
-    };
-
-    /**
      * TODO: Use a priority queue. Till then this comparator is 
      * used to find the least 'maxFeatures' shingles.
      */
@@ -287,5 +248,44 @@ var Jenkins = function() {
     var rot = function(x, k) {
         return (((x) << (k)) | ((x) >> (32-(k))));
     };
+};
+
+/**
+ * Calculates binary hamming distance of two base 16 integers.
+ */
+var hammingDistance = function(x, y) {
+    var a1 = parseInt(x, 16);
+    var a2 = parseInt(y, 16);
+    var v1 = a1^a2;
+    var v2 = (a1^a2)>>32;
+
+    v1 = v1 - ((v1>>1) & 0x55555555);
+    v2 = v2 - ((v2>>1) & 0x55555555);
+    v1 = (v1 & 0x33333333) + ((v1>>2) & 0x33333333);
+    v2 = (v2 & 0x33333333) + ((v2>>2) & 0x33333333);
+    var c1 = ((v1 + (v1>>4) & 0xF0F0F0F) * 0x1010101) >> 24;
+    var c2 = ((v2 + (v2>>4) & 0xF0F0F0F) * 0x1010101) >> 24;
+
+    return c1 + c2;
+};
+
+/**
+ * Calculates bit-wise similarity - Jaccard index.
+ */
+var similarity = function(x, y) {
+    var x16 = parseInt(x, 16);
+    var y16 = parseInt(y, 16);
+    var i = (x16 & y16);
+    var u = (x16 | y16);
+    return hammingWeight(i) / hammingWeight(u);
+};
+
+/**
+ * Calculates Hamming weight (population count).
+ */
+var hammingWeight = function(l) {
+    var c;
+    for(c = 0; l; c++) l &= l-1;
+    return c;
 };
 
